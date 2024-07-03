dad3af1c8dcd3b36f5224c38fd5c6a4784fd03b3
Johongir10 <shavkatovj73@gmail.com>
2024-07-03 10:39:01 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,6 @@
+alert("sizda hozir random sonlar chiqishi boshlaydi😁");
+let random=Math.floor(Math.random()*100+1);
+alert ("random orqali  son "+random);
+confirm("bu sonni 2ga kopaytirib bolmoqchimisiz");
+alert("random orqali chiqqan son " + random +"\n random sonni ikkiga kopaytidik natija " +random*2+"\n random sonni ikkiga boldik natija " +random/2)
+alert("random orqali chiqqan son " + random + "\n random sonni ikkiga qoldiq natija " +random%3+"\n random sonni uchga boldik natija " +random/3)
